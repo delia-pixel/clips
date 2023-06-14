ad1c867f926f56fbde6fa0518ca3c492b7239a59
delia-pixel <tpatriciareinedia@gmail.com>
2023-06-14 09:21:20 +0100

    tab nav

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 
+interface IModal{
+  id: string;
+  visible:boolean;
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class ModalService {
-  private visible = false;
+  private modals:IModal[] = [];
 
   constructor() { }
 
-  isModalOpen() {
-    return this.visible;
+  register(id:string){
+    this.modals.push({
+      id,
+      visible: false
+    });
   }
 
-  toggleModal() {
-    this.visible = !this.visible;
+  isModalOpen(id:string) :Boolean{
+    return Boolean(this.modals.find(elt => elt.id === id)?.visible);
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find((elt) => elt.id === id);
+    if (modal) {
+      modal.visible=!modal.visible
+    }
+  }
+
+  unregister(id: string) {
+    this.modals = this.modals.filter(elt => elt.id !== id);
   }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,11 +6,18 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
-  constructor(public modal: ModalService) {
+export class ModalComponent implements OnInit{
+  @Input() modalID = '';
+
+  constructor(public modal: ModalService, public el: ElementRef) {
+   
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal(){
-    this.modal.toggleModal();
+    this.modal.toggleModal(this.modalID);
   }
 }
